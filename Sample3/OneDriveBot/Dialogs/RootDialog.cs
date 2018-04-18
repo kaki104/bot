d66231d0b3264214c2a952c952e1e039e7f8e84b
Park Munchan <kaki104@hanmail.net>
2018-04-19 00:58:18 +0900

    OneDriveBot commit

---
+++ Sample3/OneDriveBot/Dialogs/RootDialog.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using BotAuth;
+using BotAuth.AADv2;
+using BotAuth.Dialogs;
+using BotAuth.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using OneDriveBot.Models;
+
+namespace OneDriveBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        private readonly AuthenticationOptions _authenticationOptions = new AuthenticationOptions
+        {
+            Authority = ConfigurationManager.AppSettings["aad:Authority"],
+            ClientId = ConfigurationManager.AppSettings["aad:ClientId"],
+            ClientSecret = ConfigurationManager.AppSettings["aad:ClientSecret"],
+            Scopes = new[] {"Files.Read"},
+            RedirectUrl = ConfigurationManager.AppSettings["aad:Callback"],
+            MagicNumberView = "/magic.html#{0}"
+        };
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            switch (activity.Text.ToLower())
+            {
+                case "get music files":
+                    // Using the OneDrive search API through the Microsoft Graph
+                    //var query = "https://graph.microsoft.com/v1.0/me/drive/search(q='{0}')?$select=id,name,size,webUrl&$top=5&$expand=thumbnails";
+                    var query = "https://graph.microsoft.com/v1.0/me/drive/special/music/children?$top=2";
+                    // Save the query so we can run it after authenticating
+                    context.ConversationData.SetValue("GraphQuery", query);
+                    // Forward the dialog to the AuthDialog to sign the user in and get an access token for calling the Microsoft Graph and then execute the specific action
+                    await context.Forward(new AuthDialog(new MSALAuthProvider(), _authenticationOptions),
+                        GetOneDriveMusicFiles, activity, CancellationToken.None);
+                    return;
+                case "get photo files":
+                    var q2 = "https://graph.microsoft.com/v1.0/me/drive/special/cameraroll/children?$orderby=createdDateTime desc&$filter=file/mimeType eq 'image/jpeg'&$top=5";
+                    context.ConversationData.SetValue("GraphQuery", q2);
+                    await context.Forward(new AuthDialog(new MSALAuthProvider(), _authenticationOptions),
+                        GetOneDrivePhotoFiles, activity, CancellationToken.None);
+                    return;
+            }
+
+            // calculate something for us to return
+            var length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task GetOneDrivePhotoFiles(IDialogContext context, IAwaitable<AuthResult> result)
+        {
+            // Getting the token from the Microsoft Graph
+            var tokenInfo = await result;
+
+            // Get the Documents from the OneDrive of the Signed-In User
+            var json = await new HttpClient().GetWithAuthAsync(tokenInfo.AccessToken,
+                context.ConversationData.GetValue<string>("GraphQuery"));
+
+            var root = JsonConvert.DeserializeObject<Rootobject>(json.ToString());
+            var reply = ((Activity) context.Activity).CreateReply();
+            foreach (var photo in root.value)
+            {
+                var card = new HeroCard
+                {
+                    Title = photo.name,
+                    Subtitle = $"Taken: {photo.photo?.takenDateTime}",
+                    Images = new List<CardImage>
+                    {
+                        new CardImage(photo.microsoftgraphdownloadUrl)
+                    },
+                    Buttons = new List<CardAction>
+                    {
+                        new CardAction(ActionTypes.DownloadFile, "Download File", value: photo.microsoftgraphdownloadUrl)
+                    }
+                };
+                reply.Attachments.Add(card.ToAttachment());
+            }
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+            var client = new ConnectorClient(new Uri(context.Activity.ServiceUrl));
+            await client.Conversations.ReplyToActivityAsync(reply);
+        }
+
+        private async Task GetOneDriveMusicFiles(IDialogContext context, IAwaitable<AuthResult> result)
+        {
+            // Getting the token from the Microsoft Graph
+            var tokenInfo = await result;
+
+            // Get the Documents from the OneDrive of the Signed-In User
+            var json = await new HttpClient().GetWithAuthAsync(tokenInfo.AccessToken,
+                context.ConversationData.GetValue<string>("GraphQuery"));
+
+            var root = JsonConvert.DeserializeObject<Rootobject>(json.ToString());
+            var reply = ((Activity) context.Activity).CreateReply();
+
+            foreach (var music in root.value)
+            {
+                var audioCard = new AudioCard
+                {
+                    Title = music.name,
+                    Subtitle = $"Artist: {music.audio.artist}, Genre: {music.audio.genre}",
+                    Media = new List<MediaUrl>
+                    {
+                        new MediaUrl(music.microsoftgraphdownloadUrl)
+                    },
+                    Buttons = new List<CardAction>
+                    {
+                        new CardAction(ActionTypes.OpenUrl, "Open File", value: music.webUrl)
+                    }
+                };
+                if (reply.Attachments.Any() == false) audioCard.Autostart = true;
+                reply.Attachments.Add(audioCard.ToAttachment());
+            }
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+            var client = new ConnectorClient(new Uri(context.Activity.ServiceUrl));
+            await client.Conversations.ReplyToActivityAsync(reply);
+        }
+    }
+}
---
+++ Sample3/OneDriveBot/Models/OneDriveItem.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace OneDriveBot.Models
+{
+    public class Rootobject
+    {
+        public string odatacontext { get; set; }
+        public Value[] value { get; set; }
+    }
+
+    public class Value
+    {
+        [JsonProperty("@microsoft.graph.downloadUrl")]
+        public string microsoftgraphdownloadUrl { get; set; }
+
+        public Createdby createdBy { get; set; }
+        public DateTime createdDateTime { get; set; }
+        public string cTag { get; set; }
+        public string description { get; set; }
+        public string eTag { get; set; }
+        public string id { get; set; }
+        public Lastmodifiedby lastModifiedBy { get; set; }
+        public DateTime lastModifiedDateTime { get; set; }
+        public string name { get; set; }
+        public Parentreference parentReference { get; set; }
+        public int size { get; set; }
+        public string webUrl { get; set; }
+        public Audio audio { get; set; }
+        public File file { get; set; }
+        public Filesysteminfo fileSystemInfo { get; set; }
+        public Shared shared { get; set; }
+        public Image image { get; set; }
+        public Photo photo { get; set; }
+        public Folder folder { get; set; }
+    }
+
+    public class Createdby
+    {
+        public Application application { get; set; }
+        public Device device { get; set; }
+        public User user { get; set; }
+        public Onedrivesync oneDriveSync { get; set; }
+    }
+
+    public class Application
+    {
+        public string displayName { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Device
+    {
+        public string id { get; set; }
+    }
+
+    public class User
+    {
+        public string displayName { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Onedrivesync
+    {
+        public string odatatype { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Lastmodifiedby
+    {
+        public User user { get; set; }
+        public Application application { get; set; }
+        public Device device { get; set; }
+        public Onedrivesync oneDriveSync { get; set; }
+    }
+
+
+    public class Parentreference
+    {
+        public string driveId { get; set; }
+        public string driveType { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string path { get; set; }
+    }
+
+    public class Audio
+    {
+        public string album { get; set; }
+        public string albumArtist { get; set; }
+        public string artist { get; set; }
+        public int bitrate { get; set; }
+        public int disc { get; set; }
+        public int duration { get; set; }
+        public string genre { get; set; }
+        public bool hasDrm { get; set; }
+        public bool isVariableBitrate { get; set; }
+        public string title { get; set; }
+        public int track { get; set; }
+        public int year { get; set; }
+    }
+
+    public class File
+    {
+        public Hashes hashes { get; set; }
+        public string mimeType { get; set; }
+    }
+
+    public class Hashes
+    {
+        public string sha1Hash { get; set; }
+    }
+
+    public class Filesysteminfo
+    {
+        public DateTime createdDateTime { get; set; }
+        public DateTime lastModifiedDateTime { get; set; }
+    }
+
+    public class Shared
+    {
+        public Owner owner { get; set; }
+        public string scope { get; set; }
+    }
+
+    public class Owner
+    {
+        public User user { get; set; }
+    }
+
+
+    public class Image
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Photo
+    {
+        public DateTime takenDateTime { get; set; }
+    }
+
+    public class Folder
+    {
+        public int childCount { get; set; }
+        public View view { get; set; }
+    }
+
+    public class View
+    {
+        public string viewType { get; set; }
+        public string sortBy { get; set; }
+        public string sortOrder { get; set; }
+    }
+}
